6db86a3dd2f203e6a619f28b2f71db8451b969f2
Gayathri Krishnan <gayu@Gayathris-MacBook-Air.local>
2025-07-30 17:23:53 +0100

    update methods

--- client/scripts/profile.js
+++ client/scripts/profile.js
@@ -1,18 +1,23 @@
+import { getToken } from "./helpers/helpers";
+
 document.addEventListener("DOMContentLoaded", async () => {
 
-  const response = await fetch("http://localhost:3000/users/1");
-
+  const token = getToken();
+  const response = await fetch("http://localhost:3000/users/", {
+    headers: {
+      authorization: `${token}`
+    }
+  });
   const user = await response.json();
-  console.log(user);
+
+
   document.getElementById("profile").innerHTML = `
     <p><strong>Username:  </strong> ${user.username}</p>
     <p><strong>Join Date:  </strong> ${user.join_date}</p>
     <p><strong>Is Admin:  </strong> ${user.is_admin? "Admin" : "No Admin"}</p>
     <p><strong>High Score:  </strong> ${user.high_score}</p>
     <p><strong>Games Played:  </strong> ${user.games_played}</p>
-    <p><strong>All Time Score:  </strong> ${user.all_time_score}</p>
-
-    `
+    <p><strong>All Time Score:  </strong> ${user.all_time_score}</p>`
 });
 
 
