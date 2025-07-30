b02f6759cb141ca476f2fa7e43ccbf9829444112
Tom Corley <tomcorley86@gmail.com>
2025-07-30 10:25:53 +0100

    Added auth, logger middleware and new routes.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const router = Router();
+const authenticate = require('../middleware/auth')
 const usersController = require('../controllers/users');
 
 router.post('/register', usersController.register);
 router.post('/login', usersController.login);
+router.patch('/update', authenticate, usersController.update);
+router.get('/:id', usersController.fetchUser);
 
 module.exports = router;
