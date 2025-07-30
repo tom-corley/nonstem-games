47a6f4c8a2ade26c1df9619042671dd4d4e2cffa
Gayathri Krishnan <gayu@Gayathris-MacBook-Air.local>
2025-07-30 14:28:58 +0100

    added basic update profile changes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,55 +1,57 @@
 const User = require('../models/users');
 
 async function register(req, res) {
-    try {
-      // Extract fields and create user
-      const { username, password } = req.body;
-      const new_user = await User.create({
-        username,
-        password,
-        is_admin: false
-      });
+  try {
+    // Extract fields and create user
+    const { username, password } = req.body;
+    const new_user = await User.create({
+      username,
+      password,
+      is_admin: false,
+    });
 
-      // Send user object, sanitised by model
-      res.status(201).send(new_user);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+    // Send user object, sanitised by model
+    res.status(201).send(new_user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
 async function login(req, res) {
   try {
-    const { username, password } = req.body
-    result = await User.authenticate(username, password)
-    res.status(200).send(result)
-  } catch(err) {
-    res.status(500).json({error: err.message})
+    const { username, password } = req.body;
+    result = await User.authenticate(username, password);
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
 
 async function fetchUser(req, res) {
   try {
-    const username = req.params.id
-    if (!username) throw new Error("Error passing in user_id");
-    const user = await User.cleanGetOneById(username)
-    res.status(200).send(user)
-  } catch(err) {
-    res.status(500).json({error: err.message})
+    const username = req.params.id;
+    if (!username) throw new Error('Error passing in user_id');
+    const user = await User.cleanGetOneById(username);
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
 
 async function update(req, res) {
   try {
-    const {username: newUsername } = req.body
-    const userId = req.user.id
-    const updatedUser = await User.updateUsername(userId, newUsername)
-    res.status(200).send(result)
-  } catch(err) {
-    res.status(500).json({error: err.message})
+    const { username: newUsername } = req.body;
+    const userId = req.user.id;
+    const updatedUser = await User.updateUsername(userId, newUsername);
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
 
-
 module.exports = {
-    register, login, fetchUser, update
-}                        
+  register,
+  login,
+  fetchUser,
+  update,
+};
